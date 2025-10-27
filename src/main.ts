6e07c99c2b0f436ad00ea2cf41eb41212410d8f1
guglextech <guglex.technologies@gmail.com>
2025-10-27 22:55:41 +0000

    [WIP] +> build command fixed

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
     }
   });
 
-  await app.listen(PORT);
+  await app.listen(PORT, '0.0.0.0');
   console.log(`Cleansheet ai-builder backend service running on port ${PORT}`);
 }
 
